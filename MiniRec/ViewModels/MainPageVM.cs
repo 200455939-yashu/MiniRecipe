d7b75b4f88b357715371787e371423701c0e01d2
Nina Sanchez <sanchez.jenina@gmail.com>
2021-03-11 14:40:43 +0800

    Add ViewModels

---
+++ MiniRec/ViewModels/MainPageVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+//using MiniRec.Models;
+
+namespace MiniRec.ViewModels
+{
+    public class MainPageVM : ViewModelBase
+    {
+        public ObservableCollection<String> _categorieslist = new ObservableCollection<String>();
+        public ObservableCollection<String> CategoriesList
+        {
+            get { return _categorieslist; }
+            set { SetProperty(ref _categorieslist, value); }
+        }
+
+        public MainPageVM()
+        {
+            CategoriesList = new ObservableCollection<String>();
+            CategoriesList.Add("Breakfast");
+            CategoriesList.Add("Lunch");
+            CategoriesList.Add("Dinner");
+            CategoriesList.Add("Dessert");
+        }
+    }
+}
